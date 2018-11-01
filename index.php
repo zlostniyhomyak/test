04485244003849bb09b9a74b8b5c362c993ceb0c
zlostniyhomyak <zlostniy_homyak@mail.ru>
2018-11-02 00:44:21 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<?php
+session_start();
+$user = '123';
+$pass = '123';
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.1 401 Unauthorized');
+    echo 'Текст, отправляемый в том случае,
+    если пользователь нажал кнопку Cancel';
+    exit;
+} else {
+	if (($_SERVER['PHP_AUTH_USER'] == $user) && ($_SERVER['PHP_AUTH_PW']) == $pass) {
+    		$_SESSION['name'] = $_SERVER['PHP_AUTH_USER'];
+    		$_SESSION['pw'] = $_SERVER['PHP_AUTH_PW'];
+    		header('Location: /netology/test/list.php ');
+    exit;
+	} elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+    		$_SESSION['name'] = $_SERVER['PHP_AUTH_USER'];    		
+    		header('Location: /netology/test/list.php ');
+    exit;
+
+	} else {	
+    echo "<p>ну хоть имя бы написал... а впрочем иди нахрен</p>";
+     exit;
+	}
+    setcookie($_SERVER['PHP_AUTH_USER']);
+}
+?>
