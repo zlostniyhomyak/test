87a186e1f66b080ee3883d69918a9fc1b212abda
zlostniyhomyak <zlostniy_homyak@mail.ru>
2018-11-02 00:45:04 +0545

    Update list.php

--- list.php
+++ list.php
@@ -1,3 +1,8 @@
+<?php 
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,17 +11,23 @@
 </head>
 <body>
 	<?php 
+
+session_start();
 	$dir = 'tests';
 	$filees = scandir($dir);
 	 ?>
 	 <h1>Список тестов:</h1>
+	 <h2><?php echo "Привет {$_SESSION['name']}"; ?></h2>
 		<form action="test.php"></form>
 	 	<?php 
 	 		foreach ($filees as $key => $value) {
-	 		if (($key!=0)&&($key!=1)) {	 		
-	 			print_r ('<a href="test.php?'.$value.'">'.substr($value, 0, -5).'</a><br>');
+	 		if (($key!=0)&&($key!=1)) {	 	
+	 			$j = substr($value, 0, -5);	
+	 			echo '<a href="test.php?', $value,'">', $j,'</a><br>';
 	 			}
 	 		}
-	 	?>
+	 		if (isset($_SESSION['pw'])){
+	 		echo '<a href="admin.php"> Добавить тест</a><br>';}
+	 		?>
 </body>
 </html>
